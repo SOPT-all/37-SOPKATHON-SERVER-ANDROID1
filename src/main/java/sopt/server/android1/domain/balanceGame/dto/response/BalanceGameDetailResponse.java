fc3091f35ae051832a834c2946c45dafeb0146f8
Dohun Kim <dohun8758@naver.com>
2025-11-23 06:46:47 +0900

    chore(application): switch active profile from dev to prod

--- src/main/java/sopt/server/android1/domain/balanceGame/dto/response/BalanceGameDetailResponse.java
+++ src/main/java/sopt/server/android1/domain/balanceGame/dto/response/BalanceGameDetailResponse.java
@@ -5,16 +5,16 @@ import sopt.server.android1.domain.balanceGame.entity.ECategory;
 import sopt.server.android1.domain.comment.entity.EGameOption;
 
 public record BalanceGameDetailResponse(
-    Long id,
-    String title,
-    String option1Title,
-    String option2Title,
-    boolean isLike,
-    int likeCount,
-    EGameOption memberOption,
-    long option1Total,
-    long option2Total,
-    LocalDateTime deadline,
-    ECategory category
+        Long id,
+        String title,
+        String option1Title,
+        String option2Title,
+        boolean isLike,
+        int likeCount,
+        EGameOption memberOption,
+        long option1Total,
+        long option2Total,
+        LocalDateTime deadline,
+        ECategory category
 ) {
 }
